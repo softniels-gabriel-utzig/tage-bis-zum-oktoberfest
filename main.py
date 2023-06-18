e35ec05bc5fa21b8d825efe764808b68ce8dea5e
gabriel <gabriel utzig>
2023-06-17 22:53:31 -0300

    implementing better messages and musics to the message

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-import datetime
+from datetime import datetime
+import json
+import random
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_WEBHOOK = os.getenv('BOT_WEBHOOK')
+OKTOBERFEST_DATE = os.getenv('OKTOBERFEST_DATE')
+DATE_FORMAT= os.getenv('DATE_FORMAT')
+BOT_USERNAME = os.getenv('BOT_USERNAME')
+BOT_AVATAR = os.getenv('BOT_AVATAR')
+
 def get_date_difference():
-    date_now = datetime.datetime.now()
-    oktoberfest = datetime.datetime(2023, 10, 12)
+    date_now = datetime.now()
+    oktoberfest = datetime.strptime(OKTOBERFEST_DATE, DATE_FORMAT)
     diff_date = oktoberfest - date_now
     days = diff_date.days
-    diff_date = diff_date.total_seconds()
-    hours = diff_date / 3600
-    diff_date = diff_date % 3600
-    minutes = diff_date / 60
-    diff_date = diff_date % 60
-    return (days, hours, minutes, diff_date)
+    hours = diff_date.seconds / 3600
+    minutes = (diff_date.seconds % 3600) / 60
+    return (list(map(round,[days, hours, minutes])))
 
 def get_music():
-    with open('.\musics.txt', 'r') as f:
-        musics = f.readlines()
-        music = musics[0]
-        musics = musics[1:]
-        
-    with open('.\musics.txt', 'w') as f:
-        f.writelines(musics)
-    return music
+    with open('musics.json', 'r') as json_file:
+        json_data = json.load(json_file)
+    music = random.choice(json_data)
+    return music['music'] 
+
+def get_message():
+    with open('messages.json', 'r') as json_file:
+        json_data = json.load(json_file)
+    message = random.choice(json_data)
+    return message['message'] 
 
 def main():
-    days_remaning, hours_remaning, minutes_remaning, seconds_remaning = get_date_difference()
-    txt = "Olá, faltam {days} dias, {hours} horas, {minutes} minutos e {seconds} segundos para a Softniels ir para a Oktoberfest!"
-    print(txt.format(days = round(days_remaning), hours = round(hours_remaning), minutes= round(minutes_remaning), seconds = round(seconds_remaning)))
+    days_remaning, hours_remaning, minutes_remaning = get_date_difference()
+    message = get_message()
     music = get_music()
+    data = {'content': message.format(days = round(days_remaning), 
+                               hours = round(hours_remaning),
+                                minutes= round(minutes_remaning))
+                                +'\n\n **A musica do dia é** ' + music,
+            'username': BOT_USERNAME,
+            'avatar_url': BOT_AVATAR
+            }
+    print(data)
+    x = requests.post(BOT_WEBHOOK, data = data)
+    print(x)
 
 if __name__ == '__main__':
     main()
